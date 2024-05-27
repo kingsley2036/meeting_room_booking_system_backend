bb62d987e90f381de2271834a5f0b1187f039632
wangjitao <jitao.wang@aivics.com>
2024-05-27 18:26:58 +0800

    预定管理模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,8 @@ import { LoginGuard } from './login.guard';
 import { PermissionGuard } from './permission.guard';
 import { MeetingRoomModule } from './meeting-room/meeting-room.module';
 import { MeetingRoom } from './meeting-room/entities/meeting-room.entity';
+import { BookingModule } from './booking/booking.module';
+import { Booking } from './booking/entities/booking.entity';
 
 @Module({
   imports: [
@@ -35,7 +37,7 @@ import { MeetingRoom } from './meeting-room/entities/meeting-room.entity';
           synchronize: true,
           logging: true,
           entities: [
-            User, Role, Permission,MeetingRoom
+            User, Role, Permission,MeetingRoom,Booking
           ],
           poolSize: 10,
           connectorPackage: 'mysql2',
@@ -58,6 +60,7 @@ import { MeetingRoom } from './meeting-room/entities/meeting-room.entity';
     RedisModule,
     EmailModule,
     MeetingRoomModule,
+    BookingModule,
     
   ],
   controllers: [AppController],
